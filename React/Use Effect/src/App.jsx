8f9c81300ded484a9212c645fdba2ebec4c9ce52
RitikRK96 <ritikkumar9525586511@gmail.com>
2025-05-14 00:04:18 +0530

    first commit

---
+++ React/Use Effect/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import BasicEffect from './Components/BasicEffect';
+import CounterEffect from './Components/CounterEffect';
+import FetchDataEffect from './Components/FetchDataEffect';
+import LearnUseEffect1 from './Components/LearnUseEffect1';
+import LearnUseEffect2 from './Components/LearnUseEffect2';
+
+function App() {
+
+  return (
+    <>
+      <LearnUseEffect1/>
+      <LearnUseEffect2/>
+      <BasicEffect/>
+      <CounterEffect/>
+      <FetchDataEffect/>
+    </>
+  )
+}
+
+export default App
