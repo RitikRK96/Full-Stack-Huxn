2688013cb7e89234df892ff15e336f01517fe47e
RitikRK96 <ritikkumar9525586511@gmail.com>
2025-05-16 18:13:01 +0530

    Java Script CHapter 2 Control FLow done

---
+++ JavaScript/02ControlFlow.js
@@ -0,0 +1,74 @@
+// Conditinal Statements
+// let a = 20;
+// let b = 20;
+
+// if(a>b){
+//     console.log("a is greater than b");
+// } else if(b>a){
+//     console.log("b is greater than a");
+// } else{
+//     console.log("a is equal to b");
+// }
+
+// let paasword ;
+// if(paasword == 8){
+//     console.log("Welcome");
+// } else if(paasword <= 8){
+//     console.log("Paasword is too Short");
+// } else if(paasword>8){
+//     console.log("Too Long Password & Password should be of 8 Characters");
+// } else{
+//     console.log("Please, provide a password");
+// }
+
+// Switch
+// switch(expression){
+//     case x:
+//         print
+//         break;
+//     case y:
+//         print
+//         break;
+// }
+
+// Loop
+
+// For loop
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`Ritik - The great, ${i}`);
+// }
+
+
+// let i = 0;
+
+// While Loop
+
+// while(i <= 20){
+//     console.log(`Ritik - The great, ${i}`);
+//     console.log(`Ritik - The great, ${i}`);
+//     i++
+// }
+
+// Do-while Loop
+
+// do {
+//   console.log(`Ritik - The great, ${i}`);
+//   i++;
+// } while (i <= 20);
+
+// Logical Operations
+// const a = true;
+// const b = false;
+// const c = 4;
+// 1. Logical AND (&&) True if both the operands are true
+// console.log(a && b);
+// console.log(c>2 && c<2);
+
+// console.log();
+// 2. Logical OR (||) True if even one is True
+// console.log(a || b);
+// console.log(c>2 || c<2);
+
+// console.log();
+// 3. Logical NOT (!) If True then false
+// console.log(!a);
