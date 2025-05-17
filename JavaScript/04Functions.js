8b800631a4bd3176b5fbeb260062b6783d9cf42b
RitikRK96 <ritikkumar9525586511@gmail.com>
2025-05-18 00:38:11 +0530

    setInterval and setTimeout Function

--- JavaScript/04Functions.js
+++ JavaScript/04Functions.js
@@ -1,70 +1,70 @@
 console.log("Functions");
 console.log();
 
-// function greet(){
-//     console.log("Hello from a function");
-// };
-// greet();
+function greet(){
+    console.log("Hello from a function");
+};
+greet();
 
-// function sayHello(name = "Unknown"){
-//     console.log("Hello, " + name);
-//     console.log(`Hello, ${name}`);
-// }
+function sayHello(name = "Unknown"){
+    console.log("Hello, " + name);
+    console.log(`Hello, ${name}`);
+}
 
-// sayHello("Ritik");
-// sayHello();
+sayHello("Ritik");
+sayHello();
 
-// function greet(name, cb){
-//     console.log(`Hello ${name}`);
-//     cb;
-// }
+function greet(name, cb){
+    console.log(`Hello ${name}`);
+    cb;
+}
 
-// function cb() {
-//     console.log("I am a callback function! \n");
-// }
-// greet("Ritik-TheGreat", cb)
+function cb() {
+    console.log("I am a callback function! \n");
+}
+greet("Ritik-TheGreat", cb)
 
-// greet("Ritik", function cb() {
-//     console.log("I am a callback function!");
-// })
+greet("Ritik", function cb() {
+    console.log("I am a callback function!");
+})
 
-// const person = {
-//     name: "Ritik",
-//     age: 21,
-//     greet: function () {
-//         return `Hello, My name is: ${person.name} & I am ${person.age} year old`
-//     }
-// }
+const person1 = {
+    name: "Ritik",
+    age: 21,
+    greet: function () {
+        return `Hello, My name is: ${person1.name} & I am ${person1.age} year old`
+    }
+}
 
-// console.log(person.greet());
+console.log(person1.greet());
 
-// const person = {
-//   name: "Ritik Kumar",
-//   age: 21,
-//   email: "ritikrk008@gmail.com",
-//   isSubscribed: true,
-//   hobbies: ["Reading", "Running", "Cooking"],
-//   address: {
-//     city: "Patna",
-//     state: "Bihar",
-//     country: "India",
-//   }
-// };
+const person = {
+  name: "Ritik Kumar",
+  age: 21,
+  email: "ritikrk008@gmail.com",
+  isSubscribed: true,
+  hobbies: ["Reading", "Running", "Cooking"],
+  address: {
+    city: "Patna",
+    state: "Bihar",
+    country: "India",
+  }
+};
 
-// console.log(person);
-// console.log();
+console.log(person);
+console.log();
 
-// const jsonString = JSON.stringify(person);
-// console.log(jsonString);
-// console.log();
+const jsonString = JSON.stringify(person);
+console.log(jsonString);
+console.log();
 
-// const parsedObject = JSON.parse(jsonString)
-// console.log(parsedObject);
+const parsedObject = JSON.parse(jsonString)
+console.log(parsedObject);
 
 // Year, month, day, hours, minutes, seconds, miliseconds
 
-// const currentDate = new Date(2025, 5, 18, 12, 30, 0, 0);
-// console.log(currentDate);
+const currentDate = new Date(2025, 5, 18, 12, 30, 0, 0);
+console.log(currentDate);
 
 const date = new Date();
 const year = date.getFullYear();
@@ -92,6 +92,18 @@ console.log(date.toDateString());
 console.log(date.toISOString());
 console.log(date.toLocaleDateString());
 
+// setInterval and setTimeout Function
+// setInterva: the function will re-excecute after given time
+// setTimeOut: the function will excecute after given time
 
+console.log();
 
+const interval = setInterval(() => {
+    console.log("Ritik is Great");
+    
+}, 2000);
 
+setTimeout(() => {
+    clearInterval(interval);
+    console.log("Interval Stopped");
+}, 10000);
