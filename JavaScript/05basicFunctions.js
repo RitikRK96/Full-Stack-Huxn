bb2da7d5d94a4472158c5974135252574a6e4860
RitikRK96 <ritikkumar9525586511@gmail.com>
2025-05-18 14:50:47 +0530

    JavaScript Chapter 5 Destructuring, restOperator, spread Operator, ternary Operator added

--- JavaScript/05basicFunctions.js
+++ JavaScript/05basicFunctions.js
@@ -27,3 +27,64 @@
 // }
 
 // user("hello", "byre", 5, 6,)
+
+// Destructuring: It allows us to "unpack" values from data-structures into separate distict values.
+
+// const foo = ["one", "two"]
+
+// const [ritik, yellw, green, blue] = foo
+
+// console.log(ritik);
+// console.log(yellw);
+// console.log(green);
+// console.log(blue);
+
+// // default values in destructure
+// let a, b;
+// [a = 5, b = 8] = ["one"]
+// console.log(a);
+// console.log(b);
+
+// // as functions
+// function f(){
+//     return [1,2]
+// }
+
+// let a, b;
+// [a,b] = f();
+// console.log(a);
+// console.log(b);
+
+// const person1 = {
+//     name: "Ritik Kumar",
+//     age: 22,
+//     gender: "male",
+//     country: "India"
+// }
+
+// const {gender, ritik,name, age,  country} = person1;
+
+// console.log(gender);
+// console.log(ritik);
+// console.log(name);
+// console.log(age);
+// console.log(country);
+
+// // Function Destructuring
+
+// const personf = {
+//     name: "Ritik kumar",
+//     age: 22,
+//     country: "India"
+// }
+
+// function printPersonInfo({name, age, country}) {
+//     console.log(`Name: ${name || "N/A"}`);
+//     console.log(`Age: ${age || "N/A"}`);
+//     console.log(`Country: ${country || "N/A"}`);
+// }
+// printPersonInfo(personf);
+
+// // Ternary Operator: (condition) ? (iftrue do this) : (if false do this)
+// let paas = 8;
+// paas == 8 ? console.log("Paas is strong") : console.log("Paasword is Weak");
